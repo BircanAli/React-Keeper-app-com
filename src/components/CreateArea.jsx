ed2a2f87fbbd3c90ebb4db563c23311cc54ef8ae
Birdzhan Ali <105841521+BircanAli@users.noreply.github.com>
2023-03-28 12:20:06 +0200

    kepper app compleded

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submiteNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          onChange={handleChange}
+          placeholder="Title"
+          value={note.title}
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          placeholder="Take a note..."
+          value={note.content}
+          rows="3"
+        />
+        <button onClick={submiteNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
